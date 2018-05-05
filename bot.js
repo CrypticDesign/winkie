a999702dfc7857505e3d691aa14b286bfafd1e8b
CrypticDesign <mashoobee@gmail.com>
2018-05-05 15:07:38 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -7,8 +7,9 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    message.reply('Pong!);
-    }
+    	message.reply('pong');
+  	}
 });
 
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
