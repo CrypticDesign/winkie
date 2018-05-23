6120c274c91b6345f4c1494d5bbb6b9547d39464
CrypticDesign <mashoobee@gmail.com>
2018-05-22 18:34:29 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,14 +20,10 @@ client.on('message', message => {
     	message.channel.send('Pong!');
   	}
     
-    if(msg.startsWith (prefix + 'say')) {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-        const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-        message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-        message.channel.send(sayMessage);
+    if (message.startsWith("say") == true) { //Check if the message send starts with "say"
+        var newMessage = message.replace("say ", ""); //Making a variable where "say " is removed
+        bot.sendMessage({to: channelID, message:newMessage
+    }); //Send the new variable.
     
     if (msg.startsWith (prefix + 'fortune')) {
         fortuneMessage = message.content.slice (9);
