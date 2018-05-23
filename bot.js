46f15c562c82288931a7862a70244413dca5a7b4
CrypticDesign <mashoobee@gmail.com>
2018-05-22 19:58:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,6 +20,17 @@ client.on('message', message => {
         message.channel.send('Pong! ' + client.ping + "ms");
     }
     
+    if (msg.startsWith (prefix + 'fortune')) {
+        fortuneMessage = message.content.slice (9);
+        number = 2;
+        var random = Math.floor (Math.random() * (number - 1 + 1)) + 1; 
+        switch (random) {
+            case 1: message.channel.send('The fortune teller has found out your answer is **yes!**!'); break;
+            case 2: message.channel.send('The fortune teller has found out your answer is **no**!'); break;
+        }
+    }
+    
+    
 });
 
 // THIS  MUST  BE  THIS  WAY
