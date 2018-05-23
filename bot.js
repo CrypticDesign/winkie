34e11ad10092c601f9b0d2940b5f9ec1edd84bc1
CrypticDesign <mashoobee@gmail.com>
2018-05-22 19:37:25 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,44 +1,20 @@
-const prefix = "w!";
-const discord = require ('discord.js');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-var client = new discord.Client(); 
+let prefix = 'w!';
 
-const token = "NDEyMzMwMzY2MDE2MjkwODI3.DeZAmw.MAFKh0rkDhHsyQ73E3zsmj8_jWs";
-
-client.on('ready', () => {
-    console.log('I am ready mate!');
-
-});
+function commandIs(str, msg){
+  return msg.content.toLowerCase().startsWith(prefix + str);
+}
 
 client.on('message', message => {
-    
-    if (message.author.bot) return;
-    
-    msg = message.content.toLowerCase ();
-    
-    if (msg.startsWith (prefix + 'ping')) {
-    	message.channel.send('Pong!');
-  	}
-    
-    if (msg.startsWith (prefix + 'fortune')) {
-        fortuneMessage = message.content.slice (9);
-        number = 2;
-        var random = Math.floor (Math.random() * (number - 1 + 1)) + 1;
-        switch (random) {
-        case 1: message.channel.send ('Winkie the fortune teller says **yes!**'); break;
-        case 2: message.channel.send ('Winkie the fortune teller says **no!**'); break;
-        }
-     
-     if (msg.startsWith (prefix + 'say')) {
-         const embed = new Discord.RichEmbed()
-         .setColor(0x954D23)
-         .setTitle("Commands List:")
-         .addField("w!help", "Will give you the list of commands")
-         .addField("w!ping", "Pong!")
-         .addField("w!fortune [question]", "Get your fortune!")
-         message.channel.send({embed});
-     }
+
+  const args = message.content.split(' ').slice(1).join(' ');
+  
+  if(commandIs('ping', message)) {
+    message.channel.send('Pong!');
+  };
+  
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login (token);
+client.login('NDQ2NTA5NDQ0MDc2MDExNTIw.DeHQvw.xFWs_K6vgnD-gsZKns3Nv0z2lsQ;');
